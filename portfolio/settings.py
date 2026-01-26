cccd262e0de701aaf3a9fbd768abf58dcdbed57e
Souleimane <souleimane.ch.ahmed@gmail.com>
2026-01-27 02:39:33 +0300

    cleaning settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,39 +1,29 @@
-"""
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/5.2/ref/settings/
-"""
-
-
 from pathlib import Path
 import os
-import dj_database_url
-from pathlib import Path
 import datetime
- 
 import environ
+import dj_database_url
+
+# 1. Initialize Environ
+env = environ.Env()
+# Read .env if it exists (useful for local dev)
+environ.Env.read_env(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# 2. Security: Never default SECRET_KEY or DEBUG to insecure values in production
+SECRET_KEY = env('SECRET_KEY') # Will raise error if missing in Railway variables
+DEBUG = env.bool('DEBUG', default=False)
 
-
-
-
-
-
-SECRET_KEY = os.environ.get('SECRET_KEY', '7pn!ofrjw0q)9l#g!0e42he%(7qyd#yh0-i$v^q5%lgr') # <-- need to look this up 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
-
-ALLOWED_HOSTS = ['*'] 
-
+# 3. Allowed Hosts: Who can VISIT this Django app?
+# This strictly needs to be the Railway URL (where Django lives)
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    '.up.railway.app' # Covers your Railway generated URL
+]
 
 # Application definition
-
 INSTALLED_APPS = [
     'modeltranslation',
     'django.contrib.admin',
@@ -47,10 +37,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'markdownx',
+    'cloudinary', # Moved up to group 3rd parties together
 
     # apps 
     'core',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -66,22 +56,31 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# 4. CORS: Who can ASK for data? (Frontend Domains)
+# We disable "Allow All" for security.
+CORS_ALLOW_ALL_ORIGINS = False 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://souleimane.com',
+    'https://www.souleimane.com',
+]
+
+# 5. CSRF: Trusted origins for POST requests
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://souleimane.com',
+    'https://www.souleimane.com',
     'https://*.up.railway.app',
 ]
-
 
 ROOT_URLCONF = 'portfolio.urls' 
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,11 +92,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'# <-- need to look this up 
+WSGI_APPLICATION = 'portfolio.wsgi.application'
 
-
-
-
+# 6. Database Configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -109,32 +106,17 @@ database_url = os.environ.get("DATABASE_URL")
 if database_url:
     DATABASES["default"] = dj_database_url.parse(database_url) 
 
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-
-
-
+# 7. Localization
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 LANGUAGES = [
@@ -144,26 +126,30 @@ LANGUAGES = [
 ]
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
 
-
-
-
-
-
+# 8. Static & Media Files
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# 9. Storage Settings (Optimized for Railway/WhiteNoise)
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        # Using CompressedManifest enables hashing and caching for better performance
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # Markdownx settings
 MARKDOWNX_IMAGE_MAX_SIZE = {'size': (4000, 4000), 'quality': 100}
 MARKDOWNX_MEDIA_PATH = datetime.datetime.now().strftime('markdownx/%Y/%m/%d')
-MARKDOWNX_UPLOAD_MAX_SIZE = 50 * 1024 * 1024 # 50 MB
+MARKDOWNX_UPLOAD_MAX_SIZE = 50 * 1024 * 1024 
 MARKDOWNX_UPLOAD_CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml']
-
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
@@ -174,16 +160,4 @@ CLOUDINARY_STORAGE = {
        'unique_filename': False,
     }
 }
-print(env('CLOUDINARY_API_KEY'))
 
-# STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
